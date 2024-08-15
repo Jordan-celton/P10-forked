c8f2900d8f608b07d5622938aae9f6f66d37f336
Dijoyce <celton.jordan@outlook.fr>
2024-08-15 15:48:14 +0200

    add connection profile and user page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,31 +1,71 @@
-import React from "react";
+// Login.js
+import React, { useState } from "react";
+import { useNavigate, Navigate } from "react-router-dom";
 import "../styles/Login.css";
 
 function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const navigate = useNavigate();
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+
+    if (username && password) {
+      setIsLoggedIn(true);
+      navigate("/user");
+    }
+  };
+
+  const handleSignOut = () => {
+    setIsLoggedIn(false);
+    navigate("/"); // Redirection vers la page de connexion
+  };
+
+  if (isLoggedIn) {
+    return (
+      <div>
+        <h1>Welcome back, {username}!</h1>
+        <button onClick={handleSignOut} className="sign-out-button">
+          Sign Out
+        </button>
+        <Navigate to="/user" />
+      </div>
+    );
+  }
+
   return (
     <main className="main bg-dark">
       <section className="sign-in-content">
         <i className="fa fa-user-circle sign-in-icon"></i>
         <h1>Sign In</h1>
-        <form>
+        <form onSubmit={handleLogin}>
           <div className="input-wrapper">
             <label htmlFor="username">Username</label>
-            <input type="text" id="username" />
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
           </div>
           <div className="input-wrapper">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" />
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
           <div className="input-remember">
             <input type="checkbox" id="remember-me" />
             <label htmlFor="remember-me">Remember me</label>
           </div>
-          {/* PLACEHOLDER DUE TO STATIC SITE */}
-          <a href="./user.html" className="sign-in-button">
+          <button type="submit" className="sign-in-button">
             Sign In
-          </a>
-          {/* SHOULD BE THE BUTTON BELOW */}
-          {/* <button type="submit" className="sign-in-button">Sign In</button> */}
+          </button>
         </form>
       </section>
     </main>
