f05cc146e60c41180974942330499a0d5578a8a5
Dijoyce <celton.jordan@outlook.fr>
2024-08-16 15:53:54 +0200

    configure redux wip

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "../src/styles/Global.css";
 
+// Importer Provider et le store configuré
+import { Provider } from "react-redux";
+import store from "../src/redux/store";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
