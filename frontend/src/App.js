277ec4cf03a40debbe42914cc600e4be7cd8afb0
Dijoyce <celton.jordan@outlook.fr>
2024-08-16 11:44:47 +0200

    add error page wip

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Navigation from "./components/Navigation/Navigation";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import User from "./pages/User";
+import Error from "./pages/Error";
 import Footer from "./components/Footer/Footer";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/user" element={<User />} />
+        <Route path="*" element={<Error />} />
       </Routes>
       <Footer />
     </Router>
---
+++ frontend/src/pages/Error.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/Error.css";
+
+const Error = () => {
+  return (
+    <main className="main bg-dark">
+      <section className="error-content">
+        <h1>404</h1>
+        <p>Page not found</p>
+      </section>
+    </main>
+  );
+};
+
+export default Error;
